b87e693f3b27f36e545706c19065b7ff02abf1ae
nimplay <nimrod7day@gmail.com>
2022-08-03 13:42:34 -0400

    add calculator logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,35 +1,51 @@
-/* eslint-disable react/prefer-stateless-function */
+import React, { useState } from 'react';
+import CalcButton from './buttons';
+import calculate from '../logic/calculate';
+import '../index.css';
 
-import React from 'react';
+function Calculator() {
+  const [calculatorObj, setCalculatorObj] = useState({});
 
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="wrapper">
-          <div className="screen"> </div>
-          <div className="btn light-gray">AC</div>
-          <div className="btn light-gray">%</div>
-          <div className="btn light-gray">+/-</div>
-          <div className="btn orange">&divide;</div>
-          <div className="btn">7</div>
-          <div className="btn">8</div>
-          <div className="btn">9</div>
-          <div className="btn orange">x</div>
-          <div className="btn">4</div>
-          <div className="btn">5</div>
-          <div className="btn">6</div>
-          <div className="btn orange">+</div>
-          <div className="btn">1</div>
-          <div className="btn">2</div>
-          <div className="btn">3</div>
-          <div className="btn orange">-</div>
-          <div className="btn zero">0</div>
-          <div className="btn">.</div>
-          <div className="btn orange">=</div>
-        </div>
-      </div>
-    );
+  const handleCalculate = (calcObj, text) => {
+    setCalculatorObj(calculate(calcObj, text));
+  };
+
+  let display = 0;
+
+  if (Object.keys(calculatorObj).length === 0 || (calculatorObj.total === null
+    && calculatorObj.next === null
+    && calculatorObj.operation === null)) {
+    display = 0;
+  } else {
+    display = calculatorObj.next !== null ? calculatorObj.next : calculatorObj.total;
   }
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="screen Calculator-result">
+          {display}
+        </div>
+        <CalcButton buttonName="AC" classButton="btn light-gray" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="+/-" classButton="btn light-gray" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="%" classButton="btn light-gray" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="÷" classButton="btn orange" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="7" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="8" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="9" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="x" classButton="btn orange" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="4" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="5" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="6" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="+" classButton="btn orange" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="1" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="2" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="3" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="-" classButton="btn orange" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="0" classButton="btn zero" onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="." onClick={handleCalculate} calcObj={calculatorObj} />
+        <CalcButton buttonName="=" classButton="btn orange" onClick={handleCalculate} calcObj={calculatorObj} />
+      </div>
+    </div>
+  );
 }
 export default Calculator;
